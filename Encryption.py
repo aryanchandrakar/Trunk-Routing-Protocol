1479f579f92d22f6b10014069f5e1245fe4c2e19
Aryan Chandrakar <aryan.c.2000@gmail.com>
2022-02-12 06:50:50 +0530

    Update Encryption.py

--- Encryption.py
+++ Encryption.py
@@ -1,38 +1,39 @@
 import base64
+
 from Crypto.PublicKey import RSA
 from Crypto.Cipher import PKCS1_OAEP
 import binascii
 import json
 
-keyPair = RSA.generate(3072)
-
-f=open("PrivateKey.privkey", "rb")
-priKey=RSA.importKey(f.read())
-f.close()
-
-f=open("PublicKey.pubkey", "rb")
-pubKey=RSA.importKey(f.read())
-f.close()
-
-msg = 'A message'
-encryptor = PKCS1_OAEP.new(pubKey)
-encrypted = encryptor.encrypt(msg.encode())
-print(encrypted)
-print("\n")
-
-f=open("cipher.txt", "wb")
-f.write(encrypted)
-f.close()
-f=open("cipher.txt", "rb")
-txt=(f.read())
-f.close()
-
-decryptor = PKCS1_OAEP.new(priKey)
-decrypted = decryptor.decrypt(txt)
-print('Decrypted:', decrypted)
+# keyPair = RSA.generate(3072)
+#
+# f=open("PrivateKey.privkey", "rb")
+# priKey=RSA.importKey(f.read())
+# f.close()
+#
+# f=open("PublicKey.pubkey", "rb")
+# pubKey=RSA.importKey(f.read())
+# f.close()
+#
+# msg = 'A message'
+# encryptor = PKCS1_OAEP.new(pubKey)
+# encrypted = encryptor.encrypt(msg.encode())
+# print(encrypted)
+# print("\n")
+#
+# f=open("cipher.txt", "wb")
+# f.write(encrypted)
+# f.close()
+# f=open("cipher.txt", "rb")
+# txt=(f.read())
+# f.close()
+#
+# decryptor = PKCS1_OAEP.new(priKey)
+# decrypted = decryptor.decrypt(txt)
+# print('Decrypted:', decrypted)
 
 #############################################################################################
-# REPEATED ENCRYPTION
+
 
 
 keyPair3072 = RSA.generate(3072)
@@ -42,12 +43,18 @@ keyPair1024 = RSA.generate(1024)
 pubKey1024 = keyPair1024.publickey()
 # print(f"Public key in hex:  (n={hex(pubKey1024.n)}, e={hex(pubKey1024.e)})")
 pubKeyPEM1024 = pubKey1024.exportKey()
+# f=open("Pubkey1024.pubkey", "wb")
+# f.write(pubKeyPEM1024)
+# f.close()
 print("Public key 1024: ")
-print(pubKeyPEM1024.decode('ascii'))
+print(pubKeyPEM1024)
 print("\n")
 
 # print(f"Private key in hex: (n={hex(pubKey1024.n)}, d={hex(keyPair1024.d)})")
 privKeyPEM1024 = keyPair1024.exportKey()
+# f=open("Privkey1024.privkey", "wb")
+# f.write(privKeyPEM1024)
+# f.close()
 print("Private key 1024: ")
 print(privKeyPEM1024.decode('ascii'))
 print("\n")
@@ -65,12 +72,18 @@ print("Encrypted 1024:", str(encrypted1024))
 pubKey2048 = keyPair2048.publickey()
 # print(f"Public key in hex:  (n={hex(pubKey2048.n)}, e={hex(pubKey2048.e)})")
 pubKeyPEM2048 = pubKey2048.exportKey()
+# f=open("Pubkey2048.pubkey", "wb")
+# f.write(pubKeyPEM2048)
+# f.close()
 print("Public key 2048: ")
 print(pubKeyPEM2048.decode('ascii'))
 print("\n")
 
 # print(f"Private key in hex: (n={hex(pubKey2048.n)}, d={hex(keyPair2048.d)})")
 privKeyPEM2048 = keyPair2048.exportKey()
+# f=open("Privkey2048.privkey", "wb")
+# f.write(privKeyPEM2048)
+# f.close()
 print("Private key 2048: ")
 print(privKeyPEM2048.decode('ascii'))
 print("\n")
@@ -85,12 +98,18 @@ print("Encrypted 2048:", str(encrypted2048))
 pubKey3072 = keyPair3072.publickey()
 # print(f"Public key in hex:  (n={hex(pubKey3072.n)}, e={hex(pubKey3072.e)})")
 pubKeyPEM3072 = pubKey3072.exportKey()
+# f=open("Pubkey3072.pubkey", "wb")
+# f.write(pubKeyPEM3072)
+# f.close()
 print("Public key 3072: ")
 print(pubKeyPEM3072.decode('ascii'))
 print("\n")
 
 # print(f"Private key in hex: (n={hex(pubKey3072.n)}, d={hex(keyPair3072.d)})")
 privKeyPEM3072 = keyPair3072.exportKey()
+# f=open("Privkey3072.privkey", "wb")
+# f.write(privKeyPEM3072)
+# f.close()
 print("Private key 3072: ")
 print(privKeyPEM3072.decode('ascii'))
 print("\n")
